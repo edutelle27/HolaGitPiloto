d3d1bf4bb865b5e890793ceaf593a5cc3753adbb
etelletxea <e.tellechea@edu.gva.es>
2024-10-09 12:15:38 +0200

    Primer cambio desde la rama Eduardo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! I am Edu Happy New Year!!");
+        System.out.println("Happy New Year desde la Rama Eduardo!! ");
     }
 }
